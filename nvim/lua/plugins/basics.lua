6b75a2094df4eb3d9a4e431e58137f518e31a759
tomoki.kuwata.nd <Tomoki.Kuwata@jp.nttdata.com>
2024-10-07 14:47:36 +0900

    move completion setting to another file

--- nvim/lua/plugins/basics.lua
+++ nvim/lua/plugins/basics.lua
@@ -70,61 +70,6 @@ return {
     end,
   },
   {
-    'L3MON4D3/LuaSnip',
-    build = "make install_jsregexp"
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-    },
-    config = function()
-      local cmp = require('cmp')
-      cmp.setup({
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-j>'] = cmp.mapping.scroll_docs(4),
-          ['<C-l>'] = cmp.mapping.complete(),
-          ['<C-c>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
-      })
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        }),
-        matching = { disallow_symbol_nonprefix_matching = false }
-      })
-    end
-  },
-  {
     'echasnovski/mini.nvim',
     version = false,
     config = function(_, opts)
---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    'L3MON4D3/LuaSnip',
+    build = "make install_jsregexp"
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = {
+          ["<C-n>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              return cmp.select_next_item()
+            end
+            fallback()
+          end, { 'i' }),
+          ["<C-p>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              return cmp.select_prev_item()
+            end
+            fallback()
+          end, { 'i' }),
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+    end
+  },
+}
